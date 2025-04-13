2a299263d3d94cdff0e38672ccb74d1e7e922fc4
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2025-04-13 21:53:26 +0100

    updated gitignore

--- 28-ec2/main.tf
+++ 28-ec2/main.tf
@@ -11,23 +11,24 @@ resource "aws_instance" "my-server" {
     ami = "ami-0df368112825f8d8f"
 
     # change instance type based on requirement 
-    instance_type = "t3.micro"
-    key_name = "irl-keypair"  
+    instance_type = "t3.small"
+    key_name = "eu-west-1-kp"  
 
     # django security group   
     vpc_security_group_ids = [ "sg-013ef36435de1738d" ]
     monitoring = true
 
     tags = {
-      "Name" = "my-server" 
+      "Name" = "my-django-server" 
     }
 
     # installing nginx
-    user_data = <<-EOF
-                #!/bin/bash
-                sudo apt update
-                sudo apt install nginx -y
-                EOF
+    # user_data = <<-EOF
+    #             #!/bin/bash
+    #             sudo apt update
+    #             sudo apt install nginx -y
+    #             EOF
+
 }
 
 output "my-server-public-dns" {
