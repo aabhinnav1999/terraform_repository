f5cd5be57851449c87c6c98a6bb0d362d184a53e
aabhinnav1999 <kvenkatadurgaabhinav1999@gmail.com>
2025-10-12 15:25:24 +0100

    updated outpus.tf

--- practice-all/modules/ec2_module/outputs.tf
+++ practice-all/modules/ec2_module/outputs.tf
@@ -1,4 +1,4 @@
 output "instance-public-ip" {
     description = "The public IP address of the EC2 instance"
-    value       = aws_instance.demo_instance.public_ip
+    value       = aws_instance.example.public_ip
 }
